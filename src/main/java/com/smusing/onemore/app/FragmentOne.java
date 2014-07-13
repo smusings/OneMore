638f2d063cf7b20d48334a7a055ab7a08685d0d1
smusings <etyulmenkov@yahoo.com>
2014-07-13 12:33:34 -0400

    cleaned up some, working on what I think are final touches on one end

--- src/main/java/com/smusing/onemore/app/FragmentOne.java
+++ src/main/java/com/smusing/onemore/app/FragmentOne.java
@@ -7,17 +7,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class FragmentOne extends Fragment{
 
-    //in case i want to do it differently
+    //everything we need
     Number nul;
     String ini;
     EditText frag_text;
     TextView frag_count;
-    LinearLayout frag_layout;
     Button add1;
     Button sub1;
     Button reset;
@@ -27,37 +25,55 @@ public class FragmentOne extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                      Bundle savedInstanceState) {
-
+        //inflates the xml layout
         View view = inflater.inflate(R.layout.fragment_one, container, false);
 
+        //gives the the 0 we need
         nul = 0;
         ini = nul.toString();
 
 
 
         //setup to use
-
         frag_text = (EditText) view.findViewById(R.id.fragment_text);
         frag_count = (TextView) view.findViewById(R.id.fragment_count);
-        frag_layout=(LinearLayout)view.findViewById(R.id.fragment_layout);
+        add1=(Button)view.findViewById(R.id.btn_plus_one);
+        sub1=(Button)view.findViewById(R.id.btn_minus_one);
+        reset=(Button)view.findViewById(R.id.btn_reset);
+
+        //sets our initial count to 0
         frag_count.setText(ini);
 
-        frag_text.setEnabled(false);
+        //+1
+        add1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a1 = 1;
+                String value = frag_count.getText().toString();
+                int intvalue = Integer.parseInt(value);
+                int amount = intvalue + a1;
+                frag_count.setText(Integer.toString(amount));
+            }
+        });
+        //-1
+        sub1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a1 = -1;
+                String value = frag_count.getText().toString();
+                int intvalue = Integer.parseInt(value);
+                int amount = intvalue + a1;
+                frag_count.setText(Integer.toString(amount));
+            }
+        });
+        //0
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-
-
+                frag_count.setText(ini);
+            }
+        });
         return view;
-    }
-
-    public String getBundleString(Bundle b){
-        String value=b.getString("subject");
-        return value;
-    }
-
-    public void doPositiveClick(){
-
-        final TextView chaptercount = (TextView) getView().findViewById(R.id.fragment_count);
-        Number n=0;
-        chaptercount.setText(n.toString());
     }
 }
--- src/main/java/com/smusing/onemore/app/MainActivity.java
+++ src/main/java/com/smusing/onemore/app/MainActivity.java
@@ -14,11 +14,11 @@ public class MainActivity extends FragmentActivity{
     //setup for activity
     FragmentManager fm=getSupportFragmentManager();
     FragmentOne f1;
-    FragmentTwo f2;
-    FragmentThree f3;
-    FragmentFour f4;
-    FragmentFive f5;
-    FragmentSix f6;
+    FragmentOne f2;
+    FragmentOne f3;
+    FragmentOne f4;
+    FragmentOne f5;
+    FragmentOne f6;
 
     private boolean singleViewExpand=false;
 
@@ -73,11 +73,20 @@ public class MainActivity extends FragmentActivity{
         setContentView(R.layout.activity_main);
         //assigns values to al fragments
         f1=(FragmentOne)getSupportFragmentManager().findFragmentById(R.id.fragment1);
-        f2=(FragmentTwo)getSupportFragmentManager().findFragmentById(R.id.fragment2);
-        f3=(FragmentThree)getSupportFragmentManager().findFragmentById(R.id.fragment3);
-        f4=(FragmentFour)getSupportFragmentManager().findFragmentById(R.id.fragment4);
-        f5=(FragmentFive)getSupportFragmentManager().findFragmentById(R.id.fragment5);
-        f6=(FragmentSix)getSupportFragmentManager().findFragmentById(R.id.fragment6);
+        f2=(FragmentOne)getSupportFragmentManager().findFragmentById(R.id.fragment2);
+        f3=(FragmentOne)getSupportFragmentManager().findFragmentById(R.id.fragment3);
+        f4=(FragmentOne)getSupportFragmentManager().findFragmentById(R.id.fragment4);
+        f5=(FragmentOne)getSupportFragmentManager().findFragmentById(R.id.fragment5);
+        f6=(FragmentOne)getSupportFragmentManager().findFragmentById(R.id.fragment6);
+
+        //set background color for each fragment
+        f1.getView().setBackgroundColor(getResources().getColor(R.color.CadetBlue));
+        f2.getView().setBackgroundColor(getResources().getColor(R.color.SeaGreen));
+        f3.getView().setBackgroundColor(getResources().getColor(R.color.ModerateBlue));
+        f4.getView().setBackgroundColor(getResources().getColor(R.color.ForestGreen));
+        f5.getView().setBackgroundColor(getResources().getColor(R.color.SteelBlue));
+        f6.getView().setBackgroundColor(getResources().getColor(R.color.Green));
+
 
         f1.getView().setTag(1);
 
